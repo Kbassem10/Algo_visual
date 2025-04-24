2b2aa4143ddbd202bb7bbc494993aa7ef969c5c3
AShalaby191 <Ahmed.shalaby.khaled1234@gmail.com>
2025-04-24 22:25:21 +0200

    merge sort and quick sort

---
+++ algorithms/Merge_sort.py
@@ -0,0 +1,43 @@
+def merge_sort(arr):
+    steps = []
+    n = len(arr)
+    
+    def merge(start, mid, end):
+        left = arr[start:mid+1]
+        right = arr[mid+1:end+1]
+        i = j = 0
+        k = start
+        
+        while i < len(left) and j < len(right):
+            steps.append({'compare': [start+i, mid+1+j]})
+            if left[i] <= right[j]:
+                arr[k] = left[i]
+                i += 1
+            else:
+                arr[k] = right[j]
+                steps.append({'swap': [k, mid+1+j]})
+                j += 1
+            k += 1
+            
+        while i < len(left):
+            arr[k] = left[i]
+            i += 1
+            k += 1
+            
+        while j < len(right):
+            arr[k] = right[j]
+            j += 1
+            k += 1
+    
+    def sort(start, end):
+        if start < end:
+            mid = (start + end) // 2
+            sort(start, mid)
+            sort(mid+1, end)
+            merge(start, mid, end)
+    
+    sort(0, n-1)
+    return steps
+
+arr = [64, 25, 12, 22, 11]
+merge_sort(arr)
